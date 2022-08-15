be0329838d5fbc31f544ebc92e1e86655eae4540
utkugokcen13 <utku.gokcen@hotmail.com>
2022-08-15 16:51:57 +0300

    first commit

---
+++ ToDo App/Item.swift
@@ -0,0 +1,13 @@
+//
+//  ItemModel.swift
+//  ToDo App
+//
+//  Created by Utku Gökçen on 15.08.2022.
+//
+
+import Foundation
+
+class ItemModel{
+    var title: String = ""
+    var isChecked: Bool = false
+}
